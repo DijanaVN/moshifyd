662439719e2fb301d0b0a4883811d05fe7f21c50
DijanaVN <veljanovskadijana@yahoo.com>
2022-04-27 12:49:55 +0200

    check

--- dist/index.58675ecb.js
+++
@@ -1,2 +0,0 @@
-const collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach((l=>l.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))));
-//# sourceMappingURL=index.58675ecb.js.map
